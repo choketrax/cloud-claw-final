9172c34e8392b23205ddd9d88cec222b07197261
choketrax <dl@goxyt.com>
2026-08-17 15:04:33 -0500

    Update singleton container ID to final version

--- src/container.ts
+++ src/container.ts
@@ -83,7 +83,7 @@ export class AgentContainer extends Container {
   }
 }
 
-const SINGLETON_CONTAINER_ID = 'cf-singleton-container-v4'
+const SINGLETON_CONTAINER_ID = 'cf-singleton-container-v-final'
 
 export async function forwardRequestToContainer(request: Request) {
   const objectId = env.AGENT_CONTAINER.idFromName(SINGLETON_CONTAINER_ID)
